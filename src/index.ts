c3cdefe70d35b6ca0c46aaf9e99cce0f5930ceb5
Di Pham <di.pham@pnnl.gov>
2023-06-12 13:32:28 -0700

    Adding blueprint dependencies and imports

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,7 @@ export { AreaHighlight } from "./components/AreaHighlight";
 export { PdfLoader } from "./components/PdfLoader";
 
 export * from "./types";
+
+import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
